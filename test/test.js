03b7ec493f033c3dbaef81e4012d0f9292b2a1b5
tim <will221223>
2021-08-06 17:00:37 +0800

    :zap:updated test file

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+// var app = require("app");
+
+describe("test 1", function(){
+  
+  
+  it("check return true", function(){
+    
+      var result = true
+      assert.ok(result);
+  })
+  
+  }
+        
+)
